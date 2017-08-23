757edddd14f8fcdd2813dc9fbbcfa7e65fd4f2e7
Marcel Schmidt <marcel.schmidt@wooga.net>
2017-08-23 19:40:07 +0200

    Added Windows UI

---
+++ GPX2Cruiser.Windows/MainWindow.Designer.cs
@@ -0,0 +1,38 @@
+﻿namespace GPX2Cruiser.Windows
+{
+  partial class MainWindow
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose (bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose ();
+      }
+      base.Dispose (disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+
+
+    #endregion
+
+    private System.Windows.Forms.Button open_file;
+    private System.Windows.Forms.Label status_route;
+    private System.Windows.Forms.Label status_wp;
+    private System.Windows.Forms.Button save_file;
+    private System.Windows.Forms.CheckBox option_hw;
+    private System.Windows.Forms.CheckBox option_tr;
+    private System.Windows.Forms.CheckBox option_fr;
+  }
+}
+
